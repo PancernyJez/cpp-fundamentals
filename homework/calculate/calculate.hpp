48cb2c9d96dc64d61144e0ca9060739c7a6c0d9a
PancernyJez <bartosz.janek@outlook.com>
2024-06-24 15:51:39 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,20 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+    if (command == "add"){
+        int wynik = first + second;
+        return std::to_string (wynik);
+    } else if (command == "substract"){
+        int wynik = first - second;
+        return std::to_string (wynik);
+    } else if (command == "multiply"){
+        int wynik = first * second;
+        return std::to_string (wynik);
+    } else if (command == "divide"){
+        float wynik = (float ) first / second;
+        return std::to_string (wynik);
+    } else return "Invalid data";
 }
