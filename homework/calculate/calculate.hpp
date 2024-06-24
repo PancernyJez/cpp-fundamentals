26f5da697d1110e67302b2cbc0361dc132fa1e4b
PancernyJez <bartosz.janek@outlook.com>
2024-06-24 16:06:58 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,6 +1,6 @@
 std::string calculate(const std::string& command, int first, int second) {
 
-    if (command == "add"){
+    if (command == "add"){ 
         int wynik = first + second;
         return std::to_string (wynik);
     } else if (command == "substract"){
