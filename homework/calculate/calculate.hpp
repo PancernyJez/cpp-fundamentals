bc4535b6c8c4ba430f36c1792a1ca26baf7446f5
PancernyJez <bartosz.janek@outlook.com>
2024-06-25 22:53:52 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,23 +1,22 @@
 #pragma once
 #include <string>
-
 std::string calculate(const std::string& command, int first, int second) {
     if (command == "add") {
-    int wynik = first + second;
-    return std::to_string(wynik);
-  } else if (command == "subtract") {
-    int wynik = first - second;
-    return std::to_string(wynik);
-  } else if (command == "multiply") {
-    int wynik = first * second;
-    return std::to_string(wynik);
-  } else if (command == "divide") {
-    if (second == 0) {
-      return "Division by 0";
-    }
-    int wynik = first / second;
-    return std::to_string(wynik);
-  } else
-    return "Invalid data";
+        int wynik = first + second;
+    	return std::to_string(wynik);
+    } else if (command == "subtract") {
+    	int wynik = first - second;
+    	return std::to_string(wynik);
+    } else if (command == "multiply") {
+    	int wynik = first * second;
+    	return std::to_string(wynik);
+    } else if (command == "divide") {
+        if (second == 0) {
+    	    return "Division by 0";
+        }
+        int wynik = first / second;
+        return std::to_string(wynik);
+    } else
+        return "Invalid data";
     return "";
 }
