e1993fb70d494be0481a212861e00bab7c7e31e3
PancernyJez <bartosz.janek@outlook.com>
2024-06-27 17:08:48 +0200

    Update nwdNww3.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,22 +2,20 @@
 
 int NWD(int lhs, int rhs) {
   int dzielnik {1};
-  int cos = 1;
-  if (abs(lhs) == abs(rhs)){return abs(lhs);}
+  int cos;
+  
+  if (lhs == 0) {return abs(rhs);}
+  else if (rhs == 0) {return abs(lhs);}
+
   while (dzielnik < abs(lhs) || dzielnik < abs(rhs)) {
-    if (lhs % dzielnik == 0 && rhs % dzielnik == 0) {
-      cos = dzielnik;
-      dzielnik++;
-    }
-    else if (lhs % dzielnik != 0 || rhs % dzielnik != 0) {
-      dzielnik++;
-    }
-  }
-  return cos;
+    if (lhs % dzielnik == 0 && rhs % dzielnik == 0) {cos = dzielnik; dzielnik++;}
+    else if (lhs % dzielnik != 0 || rhs % dzielnik != 0) {dzielnik++;}
+
+  }return cos;
 }
 
 int NWW(int lhs, int rhs) {
-  if (lhs == 0 || rhs == 0){return -1;}
+  if (abs(lhs)== abs(rhs)){return abs(lhs);}
   else {
     int x = NWD(abs(lhs), abs(rhs));
     return lhs * rhs / x;
