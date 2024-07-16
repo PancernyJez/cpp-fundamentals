f3dbe055880a74b3bdce1d7eaba6bae8f1ed5b9e
PancernyJez <bartosz.janek@outlook.com>
2024-07-16 22:24:26 +0200

    Update vectorFunctions.cpp

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,36 +1,44 @@
 #include "vectorFunctions.hpp"
-
+#include <iostream>
+#include <memory>
+#include <vector>
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec;
-    if (count > 0) {
+    if (count > 0){
         for (int i = 0; i < count; i++) {
             vec.push_back(std::make_shared<int>(i));
         }
         return vec;
-    } else {vec.push_back(std::make_shared<int>(0));
-        return vec;
+    }
+    else return vec;
+}
+
+void print(const std::vector<std::shared_ptr<int>> &vec) {
+    if (!vec.empty()){
+        for (const auto &x: vec) {
+            std::cout << *x << '\t';
+        }
+        std::cout << std::endl;
     }
 }
 
-   void print(const std::vector<std::shared_ptr<int>>& vec){
-       for (const auto& x: vec) {
-           std::cout<<*x<<'\t';
-       }
-       std::cout<<std::endl;
-   }
+void add10(std::vector<std::shared_ptr<int>> &vec) {
+    if (!vec.empty()) {
+        for (const auto &x: vec) {
+            *x += 10;
+        }
+    }
+}
 
-  void add10(std::vector<std::shared_ptr<int>>& vec){
-      for (const auto& x: vec) {
-          *x+=10;
-      }
-  }
+void sub10(int* const ptr) {
+    if (ptr != nullptr)
+        *ptr -= 10;
+}
 
-  void sub10(std::shared_ptr<int>& ptr){
-        *ptr-=10;
-  }
-
-  void sub10(std::vector<std::shared_ptr<int>>& vec){
-      for (std::shared_ptr<int> x: vec) {
-          sub10(x);
-      }
-  }
+void sub10(std::vector<std::shared_ptr<int>> &vec) {
+    if (!vec.empty()) {
+        for (std::shared_ptr<int>& ptr: vec) {
+            sub10(ptr.get());
+        }
+    }
+}
